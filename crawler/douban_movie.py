2f3d4f036d522676461d5236f4eba6c71432116a
davidleexyz <leeqcn@sina.com>
2017-02-15 11:22:09 +0800

    douban movie crawler

--- crawler/douban_movie.py
+++ crawler/douban_movie.py
@@ -3,6 +3,7 @@ features as follow:
 a) download throttle
 b) proxy pool
 c) limit crawl depth
+d) handle mixed url
 '''
 import urllib.request
 import urllib.error
@@ -16,7 +17,7 @@ from queue import Queue
 
 class Downloader():
 
-	def __init__(self, delay = 5, proxies = None, user_agent, cache = None):
+	def __init__(self, user_agent, delay =5, proxies = None, cache = None):
 		self.throttle = Throttle(delay)
 		self.proxies = proxies
 		self.user_agent = user_agent
@@ -35,7 +36,9 @@ class Downloader():
 			self.throttle.wait(url)
 			proxy = random.choice(self.proxies) if self.proxies else None
 			headers = {"User-agent": self.user_agent}
-			result = download(url, headers, proxy)
+			result = self.download(url, headers, proxy)
+
+		return result
 
 
 	def download(self, url, headers, proxy):
@@ -62,7 +65,7 @@ class Throttle():
 		domain = urllib.parse.urlparse(url).netloc
 		last_accessed = self.domains.get(domain)
 
-		if self.delay > 0 and not last_accessed:
+		if self.delay > 0 and last_accessed is not None:
 			sleep_secs = self.delay - (datetime.datetime.now() - last_accessed).second
 
 			if sleep_secs > 0:
@@ -70,39 +73,59 @@ class Throttle():
 
 		self.domains[domain] = datetime.datetime.now()
 
+
 class Parser():
 	def __init__(self):
-		#self.url = url
-		#self.soup = BeautifulSoup(html, "html.parser")
+		pass
 
 	def parse(self, url, html):
 		components = urllib.parse.urlsplit(url)
+		html = html.decode('utf8')
 		soup = BeautifulSoup(html, "html.parser")
-		tags = self.soup.find_all("table", class_="tagCol")
-		for tag in tags:
-			tag_link = tag.find_all('a')['href']
-			tag_link = components.scheme + components.netloc + tag_link
-			print(tag_link)
+		links = []
+		img_links = []
+		tags = soup.find_all("table", class_="tagCol")
+		for index in range(len(tags)):
+			tag_links = tags[index].find_all('a')
+			for tag_link in tag_links:
+				tag_href = tag_link['href']
+				tag_href = components.scheme + "://" + components.netloc + tag_href
+				print(tag_href)
+				links.append(tag_href)
 
-		items = self.soup.find_all("a", class_='nbg')
+		items = soup.find_all("a", class_='nbg')
 		for item in items:
 			href = item['href']
 			title = item['title']
 			img_src = item.img['src']
 			print(href, title, img_src)
+			links.append(href)
+			img_links.append(img_src)
 
+		return links, img_links
 
 if __name__ == "__main__":
+	max_depth = 5
 	base_url = "https://movie.douban.com/tag"
 	user_agent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.106 Safari/537.36"
 	crawl_queue = Queue()
-	crawl_queue.put(base_url)
+	crawl_queue.put((base_url, 0))
 	visited_url = []
+	parser = Parser()
 
 	while not crawl_queue.empty():
-		url = crawl_queue.get()
-		downloader = Downloader(user_agent)
-		html = downloader(url)
+		item = crawl_queue.get()
+		url = urllib.parse.quote(item[0], safe='/:?=') # handle english and chinese mixed url
+		depth = item[1]
+		d = Downloader(user_agent)
+		html = d(url)
+		visited_url.append(url)
 		if html:
-			Parser().parse(url, html)
+			links, img_links = parser.parse(url, html)
+			for link in links:
+				if link not in visited_url and depth < max_depth:
+					crawl_queue.put((link, depth+1))
+
+
+
 
